d9f60974af47e001fa3456144642ad6ec6ea60fe
Maciej Gomółka <maciej.gomolka@protonmail.com>
2024-05-09 17:18:31 +0200

    Add snapshot test

---
+++ JokesAppTests/JokeViewSnapshotTests.swift
@@ -0,0 +1,16 @@
+import SnapshotTesting
+import XCTest
+
+final class JokeViewSnapshotTests: XCTestCase {
+
+  func test_JokeView_DisplaysJoke() {
+    let joke = Joke(
+      setup: "The punchline often arrives before the set-up.",
+      punchline: "Do you know the problem with UDP jokes?"
+    )
+    let sut = JokeView(joke: joke)
+    let controller = UIHostingController(rootView: sut)
+    assertSnapshot(of: controller, as: .image(on: .iPhone13Pro))
+  }
+
+}
--- JokesAppTests/JokesAppTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-final class JokesAppTests: XCTestCase {
-
-  func testExample() {
-    XCTAssertEqual(1 + 1, 2)
-  }
-
-}
